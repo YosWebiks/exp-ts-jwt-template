f2e8af47f1f57459872732855282189ec50af929
Yosi <Yosi@YosiWebiks.com>
2024-10-09 07:22:33 +0300

    first innitial strucure commit

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import { connect } from 'mongoose'
+export const connectDB = async ():Promise<void> => {
+    try {
+        await connect(process.env.DATABASE_URL!)
+        console.log('Successfully connected to the database')
+    } catch (err) {
+        console.log('Error connecting to the database')
+        console.log(err)
+        process.exit()
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config'
+
+import express, {
+    Express,
+    Request,
+    Response,
+    ErrorRequestHandler,
+} from 'express'
+
+import { connectDB } from './config/db'
+
+// Constants
+const PORT = process.env.PORT || 3000
+
+// Connect to database
+connectDB()
+
+// App
+const app: Express = express()
+// parse request bodies (req.body)
+app.use(express.json())
+// Serving static assets
+app.use(express.static('public'))
+
+// API Routes
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send({ message: 'Welcome to My Express App API.' })
+})
+
+
+/* Error handler middleware */
+app.use(((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    console.error(err.message, err.stack)
+    res.status(statusCode).json({ message: err.message })
+
+    return
+}) as ErrorRequestHandler)
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+})
